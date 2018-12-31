742704d154bd325b3a95f5568adbc53df812fe51
Earthyang <17854217061@163.com>
2018-12-31 23:23:21 +0800

    员工考勤管理系统

---
+++ src/main/java/com/hrms/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.hrms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author yang
+ * @date 2018/12/29.
+ */
+@Controller
+@RequestMapping(value = "/hrms")
+public class MainController {
+}
